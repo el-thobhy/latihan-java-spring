c7c9ba3076bf30f5e8b5a426b55d90c868fa35bd
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-05-31 00:09:11 +0700

    first commit

---
+++ src/main/java/com/elthobhy/latihanapi/LatihanapiApplication.java
@@ -0,0 +1,13 @@
+package com.elthobhy.latihanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elthobhy/latihanapi/LatihanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elthobhy.latihanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
